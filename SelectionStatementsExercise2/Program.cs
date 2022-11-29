da02a1124f3bd9d6e6f5e8cf40c1a9b695dd487d
nathan stephens <nstephens1234567@gmail.com>
2022-11-29 10:23:06 -0600

    initial commit

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite subject in school?");
+            var subject = Console.ReadLine();
+            
+            switch (subject.ToLower()) 
+            {
+                    case "English":
+                    Console.WriteLine("English can be tedious.");
+                    break;
+                    case "History":
+                    Console.WriteLine("History was my favorite subject too!");
+                    break;
+                    case "Math":
+                    Console.WriteLine("Math is a pretty difficult subject.");
+                    break;
+                    case "Reading":
+                    Console.WriteLine("Reading is great for your imagination!");
+                    break;
+                    case "Science":
+                    Console.WriteLine("Science is really fun!!");
+                    break;  
+                    default: Console.WriteLine($"Interesting. I don't know that subject, but {subject} sure sounds neat.");
+                    break;  
+            }       
         }
     }
 }
